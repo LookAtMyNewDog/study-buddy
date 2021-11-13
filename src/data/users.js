a91b1e52d875219296ced3a5a5bee4412b74c72d
LAMND <rafaello580@gmail.com>
2021-11-13 20:35:49 +0100

    after lesson 04 commit

---
+++ src/data/users.js
@@ -0,0 +1,37 @@
+export const users = [
+  {
+    name: 'Adam Romański',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Krzysztof Batko',
+    attendance: '23%',
+    average: '3.3',
+  },
+  {
+    name: 'Patrycja Gonciarz',
+    attendance: '45%',
+    average: '4.5',
+  },
+  {
+    name: 'Olga Hahn',
+    attendance: '36%',
+    average: '3.8',
+  },
+  {
+    name: 'Paweł Andrzejewski',
+    attendance: '67%',
+    average: '4.1',
+  },
+  {
+    name: 'Szymon Knuth',
+    attendance: '20%',
+    average: '2.1',
+  },
+  {
+    name: 'Krystian Foczpaniak',
+    attendance: '85%',
+    average: '4.9',
+  },
+];
---
+++ src/views/Root.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import UsersList from '../components/UsersList/UsersList.js';
+
+const Root = () => (
+  <React.Fragment>
+    <UsersList />
+  </React.Fragment>
+);
+
+export default Root;
