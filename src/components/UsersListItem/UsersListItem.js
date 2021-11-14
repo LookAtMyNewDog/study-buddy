541cff71edabf7856804746d040ea4d031f8163f
LAMND <rafaello580@gmail.com>
2021-11-14 11:07:41 +0100

    initial commit after changes

---
+++ src/components/UsersListItem/UsersListItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './UsersListItem.module.scss';
+
+const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => (
+  <li className={styles.user}>
+    <div>{average}</div>
+    <div>
+      <p>{name}</p>
+      <p>{attendance}</p>
+    </div>
+    <button>X</button>
+  </li>
+);
+
+UsersListItem.propTypes = {
+  userData: PropTypes.shape({
+    average: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    attendance: PropTypes.string,
+  }),
+};
+
+export default UsersListItem;
