904faf660eb6346a56e93168dd97bc93682870b8
LAMND <rafaello580@gmail.com>
2021-11-19 20:35:00 +0100

    create styled components

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './UsersListItem.module.scss';
+import Button from '../../atoms/Button/Button';
+import { Wrapper } from './UsersListItem.styles';
 
 const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => (
-  <li className={styles.user}>
+  <Wrapper>
     <div>{average}</div>
     <div>
       <p>{name}</p>
       <p>{attendance}</p>
     </div>
-    <button>X</button>
-  </li>
+    <Button />
+  </Wrapper>
 );
 
 UsersListItem.propTypes = {
